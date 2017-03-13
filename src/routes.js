7f43b3fd110a56d80b7a1ddd6c93788506f486f9
aborozduhin <aleksandr.borozduhin@artezio.com>
2017-03-13 19:45:28 +0400

    base routes

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import { Router, Route, Redirect, IndexRedirect, IndexRoute, hashHistory, browse
 import Root from './RouteHandlers/Root'
 import React from 'react'
 import Home from "./RouteHandlers/Home";
+import Admin from "./RouteHandlers/Admin";
 
 export default (
     <Router history = {browserHistory}>
         <Route path = "/" component = {Root} >
             <IndexRedirect to = "home" />
             <Route path = "home" component = {Home} />
+           <Route path = "admin" component = {Admin} />
            
             {/*<Route path = "*" component = {NotFoundPage} />*/}
         </Route>
