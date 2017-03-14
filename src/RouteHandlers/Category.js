743061adcd5fa1d53052aba9ad620af5e7a3ba99
aborozduhin <aleksandr.borozduhin@artezio.com>
2017-03-14 11:41:14 +0400

    build fix

---
+++ src/RouteHandlers/Category.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Category extends Component {
+    render() {
+        return (
+            <div>
+                Category page
+            </div>
+        );
+    }
+}
+
+export default Category;
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
 import categories from "./categories";
 import wishes from "./wishes";
-import category from "./category";
+//import category from "./category";
 
 export default combineReducers({
     categories,
     wishes,
-    category
+    //category
 });
